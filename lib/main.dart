b8c006d6b48968b6528ec7ff0da47ee342ee146e
Adi <oradam86@gmail.com>
2022-07-13 15:55:01 +0200

    Magic 8 Ball

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BallPage());
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Magic 8 Ball',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const BallHomePage(
+        title: 'Magic 8 Ball Home Page',
+      ),
+    );
+  }
+}
+
+class BallHomePage extends StatefulWidget {
+  const BallHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<BallHomePage> createState() => _BallHomePageState();
+}
+
+class _BallHomePageState extends State<BallHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(widget.title),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
