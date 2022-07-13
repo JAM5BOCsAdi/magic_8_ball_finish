77e1b600c8a63007ffe77c5d16525ec4d4af01ce
Adi <oradam86@gmail.com>
2022-07-13 19:03:50 +0200

    Finish the work added a custom Elevated Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'my_elevated_button_class.dart';
 
 void main() {
   runApp(const BallPage());
@@ -33,6 +38,18 @@ class BallHomePage extends StatefulWidget {
 }
 
 class _BallHomePageState extends State<BallHomePage> {
+  int ballNumber = 1;
+
+  void imageChanger() {
+    setState(
+      () {
+        ballNumber = Random().nextInt(5) + 1; // 5: 0-4   +1 means: 1-6
+        if (kDebugMode) {
+          print("You pressed Elevated Button and the number is: $ballNumber");
+        }
+      },
+    );
+  }
 /*
   int _counter = 0;
 
@@ -61,12 +78,75 @@ class _BallHomePageState extends State<BallHomePage> {
           child: Row(
             children: <Widget>[
               Expanded(
-                child: Container(
-                  height: 200.0,
-                  margin: const EdgeInsets.symmetric(horizontal: 20.0),
-                  color: Colors.black,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+/*                    gradient: const LinearGradient(
+                      colors: [
+                        Colors.blueAccent,
+                        Colors.redAccent,
+                        Colors.purpleAccent
+                        //add more colors
+                      ],
+                    ),
+*/
+                    borderRadius: BorderRadius.circular(50.0),
+                    boxShadow: const <BoxShadow>[
+                      BoxShadow(
+                        color:
+                            Color.fromRGBO(0, 0, 0, 0.57), //shadow for button
+                        blurRadius: 10,
+                      )
+                    ],
+                  ),
+                  child: MyElevatedButton(
+                    height: 400.0,
+                    width: double.maxFinite,
+                    borderRadius: BorderRadius.circular(50.0),
+                    onPressed: () {
+                      imageChanger();
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Image.asset("images/ball$ballNumber.png"),
+                    ),
+                  ),
                 ),
+/*                // flex: 1,
+                  child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      onSurface: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                      padding: const EdgeInsets.all(20.0),
+                      side: BorderSide(
+                        color: Colors.blue.shade300,
+                      ),
+                    ),
+
+
+                      style: ButtonStyle(
+                      side: MaterialStateProperty.all(
+                        BorderSide(
+                          width: 1.0,
+                          color: Colors.blue.shade300,
+                        ),
+                      ),
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.transparent),
+                      padding: MaterialStateProperty.all(
+                        const EdgeInsets.all(16.0),
+                      ),
+                      shadowColor: MaterialStateProperty.all(
+                        const Color.fromRGBO(0, 0, 0, 0.0),
+                      ),
+                    ),
+*/
               ),
+            ],
+          ),
+        ),
 /*
               Text(
                 'You have pushed the button this many times:',
@@ -77,10 +157,7 @@ class _BallHomePageState extends State<BallHomePage> {
                 style: TextStyle(color: Colors.white, fontSize: 40.0),
               ),
 */
-            ],
-          ),
-        ),
-      ),
+
 /*
         floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
@@ -88,6 +165,7 @@ class _BallHomePageState extends State<BallHomePage> {
         child: const Icon(Icons.add),
       ),
 */
+      ),
     );
   }
 }
